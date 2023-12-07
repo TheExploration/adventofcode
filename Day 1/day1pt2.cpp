942fbe09791c6bd743f03e74930a7088074e6196
Exploration <zhangphil@outlook.com>
2023-12-07 00:23:25 +0000

    push

--- Day 1/day1pt2.cpp
+++ Day 1/day1pt2.cpp
@@ -14,22 +14,52 @@ int main()
     {
         int firstNum = -1;
         int lastNum = -1;
-
+        
         for (int i = 0; i < line.length(); i++)
         {
+            int temp=-1;
             if (isdigit(line.at(i)))
             {
                 if (firstNum == -1)
                 {
                     firstNum = line.at(i) - '0';
-                }
-            }
-            else if (line.substr(i, i + 3) == "one")
-            {
+                } 
+                lastNum = line.at(i) - '0';
                 
-
             }
+            else if (line.substr(i, 3) == "one")
+            {   
+                temp = 1;
+
+            } else if (line.substr(i,3) == "two") {
+                temp = 2;
+
+            } else if (line.substr(i, 5) == "three") {
+                temp = 3;
+            } else if (line.substr(i, 4) == "four") {
+                temp = 4;
+            } else if (line.substr(i,4) == "five") {
+                temp = 5;
+            } else if (line.substr(i,3) == "six") {
+                temp = 6;
+            } else if (line.substr(i,5) == "seven") {
+                temp = 7;
+            } else if (line.substr(i,5) == "eight") {
+                temp = 8;
+            } else if (line.substr(i,4) == "nine") {
+                temp = 9;
+            }
+
+            if (firstNum == -1) {
+                firstNum = temp;
+            } 
+            if (temp != -1) {
+                lastNum = temp;
+            }
+            
         }
+         
+        
 
         if (firstNum != -1 && lastNum != -1)
         {
@@ -37,7 +67,7 @@ int main()
         }
     }
 
-    std::cout << total << std::endl;
+    std::cout << total << endl;
 
     return 0;
 }
